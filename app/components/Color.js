e545c3352081f1a20a8ae83be4a9f3402cf2e415
Alex Efremov <alex.efremov.dev@gmail.com>
2016-08-18 13:22:13 +0300

    Add working colorPicker

--- app/components/Color.js
+++ app/components/Color.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import './../styles/color.scss';
+
 const Color = React.createClass({
+  changeColor: function() {
+    var pickedColor = this.refs.pickedColor.value;
+    var colorNum = this.props.colorNum;
+    this.props.changeColor(pickedColor, colorNum);
+  },
   render: function() {
     return (
       <div>
-        Color will be here {this.props.color}
+        <div><p>Color will be here {this.props.color}</p></div>
+        <label className="color-picker__label">Pick Color<input ref="pickedColor" className="color-picker__input" type="color" defaultValue="#ffcccc" onChange={this.changeColor} /></label>
         <button onClick={this.props.selectColor.bind(null, this.props.colorNum)}>Select color</button>
       </div>
     )
--- app/components/Palette.js
+++ app/components/Palette.js
@@ -16,8 +16,13 @@ const Palette = React.createClass({
   selectColor: function(color) {
     this.setState({selectedColor: color});
   },
+  changeColor: function(colorValue, colorNum) {
+    console.log(this.state.colors[colorNum]);
+    this.state.colors[colorNum] = colorValue;
+    this.setState({colors: this.state.colors, selectedColor: colorNum});
+  },
   renderColors: function(color, index) {
-    return <Color key={index} color={color} colorNum={index} selectColor={this.selectColor} />;
+    return <Color key={index} color={color} colorNum={index} selectColor={this.selectColor} changeColor={this.changeColor} />;
   },
   updateBackground: function() {
     document.getElementsByTagName('body')[0].style.backgroundColor = this.state.colors[this.state.selectedColor]
